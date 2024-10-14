77c6250005097a8958b7c81a2df701cb132078c9
ES-Mohamed Ali <115352232+ES-Mohamed609@users.noreply.github.com>
2024-10-14 12:07:29 +0300

    yousef's-commit

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace WinFormsApp2
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("testing github");
         }
     }
 }
